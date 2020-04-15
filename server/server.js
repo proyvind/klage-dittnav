6682cd8efb92af6d6db32a86dc1f2f2d3d445b87
Richard Borge <richard.borge@nav.no>
2020-04-15 10:59:59 +0200

    Endrer litt på server for å få den til å starte og se om det funker med nais

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const path = require("path");
 const mustacheExpress = require("mustache-express");
 const getDecorator = require("./dekorator");
 const buildPath = path.resolve(__dirname, "../build");
-const basePath = `/`;
 const server = express();
 
 server.set("views", `${__dirname}/../build`);
@@ -15,41 +14,20 @@ server.engine("html", mustacheExpress());
 
 // Parse application/json
 server.use(express.json());
-server.use((req, res, next) => {
-  res.removeHeader("X-Powered-By");
-  res.setHeader(
-    "Content-Security-Policy",
-    `script-src 'self' 'unsafe-inline' 'unsafe-eval' ${[
-      "appres.nav.no",
-      "www.nav.no",
-      "www-q1.nav.no",
-      "https://klage.dev-nav.no",
-      "https://klage.dev-gcp.nais.io",
-      "account.psplugin.com",
-      "in.taskanalytics.com",
-      "static.hotjar.com",
-      "script.hotjar.com",
-      "vc.hotjar.io",
-      "www.google-analytics.com",
-      "www.googletagmanager.com"
-    ].join(" ")}; connect-src 'self' api.amplitude.com amplitude.nav.no nav.psplugin.com tjenester.nav.no vc.hotjar.io;`
-  );
-  next();
-});
 
 // Static files
-server.use(`${basePath}`, express.static(buildPath, { index: false }));
+server.use('/', express.static(buildPath, { index: false }));
 
 // Nais functions
-server.get(`${basePath}/internal/isReady`, (req, res) =>
+server.get(`/internal/isReady`, (req, res) =>
   res.sendStatus(200)
 );
 
-server.get(`${basePath}/internal/isAlive`, (req, res) =>
+server.get(`/internal/isAlive`, (req, res) =>
     res.sendStatus(200)
 );
 
-server.get(`${basePath}/config`, (req, res) =>
+server.get(`/config`, (req, res) =>
   res.send({
     appUrl: process.env.REACT_APP_URL,
     loginserviceUrl: process.env.REACT_APP_LOGINSERVICE_URL,
@@ -71,7 +49,7 @@ server.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) =>
 );
 
 const port = process.env.PORT || 8080;
-server.listen(port, () => console.log(`App listening on port: ${port}`));
+server.listen(port, () => console.log(`App listening on: ${port}`));
 
 process.on("SIGTERM", () =>
   setTimeout(() => console.log("Har sovet i 30 sekunder"), 30000)
