d81eb9672c6a45503d3ed421058102428f7897d5
Hval <marianne.hval@nav.no>
2020-05-08 14:24:11 +0200

    Manual vedtaksform

--- src/mock-api/get/vedtak.ts
+++ src/mock-api/get/vedtak.ts
@@ -5,12 +5,14 @@ export const okVedtak: Vedtak[] = [
         title: 'Vedtak om avslag på sykepenger',
         vedtaksdato: new Date(),
         tema: 'SP',
-        enhet: 'NAV Hallingdal'
+        enhet: 'NAV Hallingdal',
+        NAV_referanse: 'Herr Kylling'
     },
     {
         title: 'Enda et vedtak om avslag på sykepenger',
         vedtaksdato: new Date(2019, 1),
         tema: 'SP',
-        enhet: 'NAV Ringerike'
+        enhet: 'NAV Ringerike',
+        NAV_referanse: 'Byggmester Bob'
     }
 ];
