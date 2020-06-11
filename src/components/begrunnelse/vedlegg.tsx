2967f4f4e456da0500e64fe133f2c6f54d62aa4b
Hval <marianne.hval@nav.no>
2020-06-11 13:29:27 +0200

    Fix layout + button press

--- src/components/begrunnelse/vedlegg.tsx
+++ src/components/begrunnelse/vedlegg.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {VEDLEGG_STATUS, VedleggProps} from "../../types/vedlegg";
-import {getDeleteVedleggUrl} from "../../clients/apiUrls";
-import Check from "../../assets/images/Check";
-import Cross from "../../assets/images/Cross";
-import Bin from "../../assets/images/Bin";
-import Lenke from "nav-frontend-lenker";
-import 'nav-frontend-tabell-style';
+import { VEDLEGG_STATUS, VedleggProps } from '../../types/vedlegg';
+import { getDeleteVedleggUrl } from '../../clients/apiUrls';
+import Check from '../../assets/images/Check';
+import Cross from '../../assets/images/Cross';
+import Bin from '../../assets/images/Bin';
+import Lenke from 'nav-frontend-lenker';
 
 interface Props {
     vedlegg: VedleggProps[];
@@ -13,12 +12,12 @@ interface Props {
 }
 
 const VedleggVisning = (props: Props) => {
-
     if (props.vedlegg.length === 0) {
         return null;
     }
 
-    const deleteVedlegg = (vedlegg: VedleggProps) => {
+    const deleteVedlegg = (event: any, vedlegg: VedleggProps) => {
+        event.preventDefault();
         if (!vedlegg.id) {
             props.deleteAction(vedlegg);
             return;
@@ -26,16 +25,18 @@ const VedleggVisning = (props: Props) => {
         // TODO: Change when klageid is known
         fetch(getDeleteVedleggUrl(1, vedlegg.id), {
             method: 'DELETE'
-        }).then(result => {
-            console.log(result);
-            props.deleteAction(vedlegg);
-        }).catch(error => {
-            console.log(error);
         })
+            .then(result => {
+                console.log(result);
+                props.deleteAction(vedlegg);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     };
 
     return (
-        <table className={'tabell'}>
+        <table className="tabell tabell--stripet">
             <thead>
                 <tr>
                     <th>Fil</th>
@@ -45,27 +46,21 @@ const VedleggVisning = (props: Props) => {
                 </tr>
             </thead>
             <tbody>
-            {Array.from(props.vedlegg).map((vedlegg: VedleggProps) => (
-                <tr>
-                    <td>
-                        {vedlegg.file.name}
-                    </td>
-                    <td>
-                        {vedlegg.status === VEDLEGG_STATUS.OK ? <Check /> : <Cross />}
-                    </td>
-                    <td>
-                        {vedlegg.message}
-                    </td>
-                    <td>
-                        <Lenke href={'#'} onClick={() => deleteVedlegg(vedlegg)}>
-                            <Bin />
-                        </Lenke>
-                    </td>
-                </tr>
-            ))}
+                {Array.from(props.vedlegg).map((vedlegg: VedleggProps, index: number) => (
+                    <tr key={index}>
+                        <td>{vedlegg.file.name}</td>
+                        <td>{vedlegg.status === VEDLEGG_STATUS.OK ? <Check /> : <Cross />}</td>
+                        <td>{vedlegg.message}</td>
+                        <td>
+                            <Lenke className="no-style" href={'#'} onClick={(e: any) => deleteVedlegg(e, vedlegg)}>
+                                <Bin />
+                            </Lenke>
+                        </td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
-}
+};
 
 export default VedleggVisning;
