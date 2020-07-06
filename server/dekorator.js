6496399ab78734b4ecb507385535ddfe52f5968d
Marianne Hval <marianne.hval@nav.no>
2020-07-06 15:35:20 +0200

    Change to simple decorator (#71)

--- server/dekorator.js
+++ server/dekorator.js
@@ -1,6 +1,6 @@
-const jsdom = require("jsdom");
-const request = require("request");
-const NodeCache = require("node-cache");
+const jsdom = require('jsdom');
+const request = require('request');
+const NodeCache = require('node-cache');
 const { JSDOM } = jsdom;
 
 const SECONDS_PER_MINUTE = 60;
@@ -8,43 +8,37 @@ const SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
 
 // Refresh cache every hour
 const cache = new NodeCache({
-  stdTTL: SECONDS_PER_HOUR,
-  checkperiod: SECONDS_PER_MINUTE,
+    stdTTL: SECONDS_PER_HOUR,
+    checkperiod: SECONDS_PER_MINUTE
 });
 
-const decoratorUrl =
-  process.env.NAIS_CLUSTER_NAME === "dev-sbs"
-    ? "https://appres-q1.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true"
-    : "https://appres.nav.no/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true&skiplinks=true&megamenu-resources=true";
+const baseUrl = process.env.REACT_APP_APPRES_CMS_URL;
+const decoratorUrl = baseUrl + '/common-html/v4/navno?simple=true';
 
 const getDecorator = () =>
-  new Promise((resolve, reject) => {
-    const decorator = cache.get("main-cache");
-    if (decorator) {
-      resolve(decorator);
-    } else {
-      request(decoratorUrl, (error, response, body) => {
-        if (!error && response.statusCode >= 200 && response.statusCode < 400) {
-          const { document } = new JSDOM(body).window;
-          const prop = "innerHTML";
-          const data = {
-            NAV_SKIPLINKS: document.getElementById("skiplinks")[prop],
-            NAV_SCRIPTS: document.getElementById("scripts")[prop],
-            NAV_STYLES: document.getElementById("styles")[prop],
-            NAV_HEADING: document.getElementById("header-withmenu")[prop],
-            NAV_FOOTER: document.getElementById("footer-withmenu")[prop],
-            MEGAMENU_RESOURCES: document.getElementById("megamenu-resources")[
-              prop
-            ],
-          };
-          cache.set("main-cache", data);
-          console.log(`Creating cache`);
-          resolve(data);
+    new Promise((resolve, reject) => {
+        const decorator = cache.get('main-cache');
+        if (decorator) {
+            resolve(decorator);
         } else {
-          reject(new Error(error));
+            request(decoratorUrl, (error, response, body) => {
+                if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+                    const { document } = new JSDOM(body).window;
+                    const prop = 'innerHTML';
+                    const data = {
+                        NAV_SCRIPTS: document.getElementById('scripts')[prop],
+                        NAV_STYLES: document.getElementById('styles')[prop],
+                        NAV_HEADING: document.getElementById('header-withmenu')[prop],
+                        NAV_FOOTER: document.getElementById('footer-withmenu')[prop]
+                    };
+                    cache.set('main-cache', data);
+                    console.log(`Creating cache`);
+                    resolve(data);
+                } else {
+                    reject(new Error(error));
+                }
+            });
         }
-      });
-    }
-  });
+    });
 
 module.exports = getDecorator;
