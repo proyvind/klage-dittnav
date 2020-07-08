1dce1ca75540511adbd41c75339a22e1a6c194a1
Hval <marianne.hval@nav.no>
2020-07-08 09:49:13 +0200

    Return value in kvitteringpage

--- src/pages/kvittering/kvittering-page.tsx
+++ src/pages/kvittering/kvittering-page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Kvittering from '../../components/kvittering/kvittering';
 
-const KvitteringPage = () => <Kvittering />;
+const KvitteringPage = () => {
+    return <Kvittering />;
+};
 
 export default KvitteringPage;
