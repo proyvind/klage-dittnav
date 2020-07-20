d5be2a871c7ba161897c4670ff004484db1edd45
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2020-07-20 15:10:06 +0200

    Redirects to app after decorator login.

--- server/dekorator.js
+++ server/dekorator.js
@@ -16,7 +16,7 @@ const decorator_base_url = 'https://appres.nav.no';
 const decorator_base_url_gcp = 'https://appres-q1.nav.no';
 
 const baseUrl = process.env.NAIS_CLUSTER_NAME === 'dev-sbs' ? decorator_base_url_gcp : decorator_base_url;
-const decoratorUrl = baseUrl + '/common-html/v4/navno?simple=true';
+const decoratorUrl = baseUrl + '/common-html/v4/navno?simple=true&redirectToApp=true';
 
 const getDecorator = () =>
     new Promise((resolve, reject) => {
