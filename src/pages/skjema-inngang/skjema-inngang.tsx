9834b4dd84d9e7afb1afee420a822e4b8513697d
Hval <marianne.hval@nav.no>
2020-07-27 14:24:53 +0200

    prettier

--- src/pages/skjema-inngang/skjema-inngang.tsx
+++ src/pages/skjema-inngang/skjema-inngang.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import InngangInfoBox from '../../components/skjema-inngang/inngang-info-box';
 import NotFoundPage from '../not-found/not-found-page';
-import {isValidYtelse} from '../../utils/routes.config';
-import {Margin80TopContainer} from '../../styled-components/main-styled-components';
-import {getYtelseFromSearch} from "../../utils/query-string";
+import { isValidYtelse } from '../../utils/routes.config';
+import { Margin80TopContainer } from '../../styled-components/main-styled-components';
+import { getYtelseFromSearch } from '../../utils/query-string';
 
 const SkjemaInngang = (props: any) => {
     const ytelse = getYtelseFromSearch(props.location.search);
