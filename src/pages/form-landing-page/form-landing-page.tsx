4a24329048281e55f4b3d2a9e693738f68ee0adb
Hval <marianne.hval@nav.no>
2020-08-05 15:10:36 +0200

    WithLoading component for loading...

--- src/pages/form-landing-page/form-landing-page.tsx
+++ src/pages/form-landing-page/form-landing-page.tsx
@@ -4,11 +4,11 @@ import { Vedtak } from '../../types/vedtak';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkAuth } from '../../store/actions';
 import { Store } from '../../store/reducer';
-import NavFrontendSpinner from 'nav-frontend-spinner';
 import MainFormPage from './main-form-page';
 import { instanceOfVedtak, elementAsVedtak } from '../../mock-api/get/vedtak';
 import NotFoundPage from '../not-found/not-found-page';
 import { isValidYtelse } from '../../utils/routes.config';
+import WithLoading from '../../components/general/loading/withLoading';
 
 const FormLandingPage = (props: any) => {
     const dispatch = useDispatch();
@@ -31,12 +31,12 @@ const FormLandingPage = (props: any) => {
         }
     }, [props.location.search, chosenYtelse]);
 
-    if (loading) {
-        return <NavFrontendSpinner type={'XL'} />;
-    }
-
     if (isValidYtelse(chosenYtelse)) {
-        return <MainFormPage ytelse={chosenYtelse} availableVedtak={availableVedtak} chosenVedtak={chosenVedtak} />;
+        return (
+            <WithLoading loading={loading}>
+                <MainFormPage ytelse={chosenYtelse} availableVedtak={availableVedtak} chosenVedtak={chosenVedtak} />
+            </WithLoading>
+        );
     }
     return <NotFoundPage />;
 };
