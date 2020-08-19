e5e61d5002d5cc4105f102adcb7546eea89dd555
Hval <marianne.hval@nav.no>
2020-08-19 10:51:56 +0200

    Scroll to top between pages

--- src/pages/begrunnelse/begrunnelse-page.tsx
+++ src/pages/begrunnelse/begrunnelse-page.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Begrunnelse from '../../components/begrunnelse/begrunnelse';
 
-const BegrunnelsePage = (props: any) => (
-    <Begrunnelse
-        chosenVedtak={props.chosenVedtak}
-        ytelse={props.ytelse}
-        next={() => props.next()}
-        previous={() => props.previous()}
-    />
-);
+const BegrunnelsePage = (props: any) => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    return (
+        <Begrunnelse
+            chosenVedtak={props.chosenVedtak}
+            ytelse={props.ytelse}
+            next={() => props.next()}
+            previous={() => props.previous()}
+        />
+    );
+};
 
 export default BegrunnelsePage;
