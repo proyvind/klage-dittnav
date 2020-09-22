8dff7c248ac0916ebbb59f334b5c7cc147384b52
Christian Skrøvseth <christian.skrovseth@nav.no>
2020-09-22 16:19:37 +0200

    Remove dead code

--- src/index.tsx
+++ src/index.tsx
@@ -4,48 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { footer, header, scripts, styles } from './mock-api/get/decorator';
-//import { setupMock } from './mock-api/setup-mock';
 import configureStore from './store/configureStore';
-import Environment, { fetchEnv, isLocalhost } from './utils/environment';
+import Environment, { fetchEnv } from './utils/environment';
 import { BrowserRouter } from 'react-router-dom';
-import { logError, logInfo } from './utils/logger/frontendLogger';
+import { logError } from './utils/logger/frontendLogger';
 
-//const mockEnabled = process.env.NODE_ENV === 'development' || process.env.REACT_APP_MOCK_DATA === 'true';
 const store = configureStore();
 
 const init = async () => {
-    if (process.env.NODE_ENV === 'development') {
-        // Mock decorator
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_HEADING}}}', header);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_FOOTER}}}', footer);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_STYLES}}}', styles);
-        document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SCRIPTS}}}', scripts);
-
-        // Execute client.js
-        let script = document.createElement('script');
-        script.src = 'https://www.nav.no/dekoratoren/client.js';
-        document.body.appendChild(script);
-    }
-    // If not i develop mode, but still want to run mock
-    //if (mockEnabled) {
-    //    setupMock();
-    //}
-
-    if (isLocalhost && process.env.NODE_ENV === 'development') {
-        logInfo('Local environment.');
-        Environment.setEnv({
-            appUrl: process.env.REACT_APP_URL!,
-            loginserviceUrl: process.env.REACT_APP_LOGINSERVICE_URL!,
-            apiUrl: process.env.REACT_APP_API_URL!
-        });
-    } else {
-        try {
-            const env = await fetchEnv();
-            Environment.setEnv(env);
-        } catch (e) {
-            logError(e);
-        }
+    try {
+        const env = await fetchEnv();
+        Environment.setEnv(env);
+    } catch (e) {
+        logError(e);
     }
 
     ReactDOM.render(
