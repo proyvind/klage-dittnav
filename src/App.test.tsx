e7a1a45521154c5c20caf053e1161a2c961ffe6a
Christian Skrøvseth <christian.skrovseth@nav.no>
2020-09-29 13:37:01 +0200

    Formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import App from "./App";
-import configureStore from "./store/configureStore";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-describe("Welcome klage", () => {
-  const store = configureStore();
+describe('Welcome klage', () => {
+    const store = configureStore();
 
-  it("Renders correctly when there is no data", () => {
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+    it('Renders correctly when there is no data', () => {
+        const tree = renderer
+            .create(
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </Provider>
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
