76bca2d0cca911d7a3fd7b2a438e42b2cdc0dd4e
Christian Skrøvseth <christian.skrovseth@nav.no>
2020-10-22 08:52:49 +0200

    Remove page title

--- src/components/general/layout.tsx
+++ src/components/general/layout.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import PageTitle from './pageTitle';
 import { MasterPaddingContainer, MainContainer } from '../../styled-components/main-styled-components';
-import { useSelector } from 'react-redux';
-import { Store } from '../../store/reducer';
 
 interface Props {
     children: React.ReactChild | React.ReactChildren;
 }
 
-const Layout = ({ children }: Props) => {
-    const { chosenYtelse } = useSelector((state: Store) => state);
-    const title = chosenYtelse !== '' ? `Klage på vedtak for ${chosenYtelse?.toLowerCase()}` : `Klage på vedtak`;
-
-    return (
-        <>
-            <PageTitle title={title} />
-
-            <MainContainer>
-                <MasterPaddingContainer>{children}</MasterPaddingContainer>
-            </MainContainer>
-        </>
-    );
-};
+const Layout = ({ children }: Props) => (
+    <MainContainer>
+        <MasterPaddingContainer>{children}</MasterPaddingContainer>
+    </MainContainer>
+);
 
 export default Layout;
--- src/components/general/pageTitle.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import PageTitle from './pageTitle';
-
-describe('Page title', () => {
-    it('should render page title', () => {
-        const wrapper = shallow(<PageTitle title="Test" />);
-    });
-});
