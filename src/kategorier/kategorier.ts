94bfdf2553d8eba667c92690cea9dac1e1870003
Marianne Hval <marianne.hval@nav.no>
2021-02-12 11:18:27 +0100

    Digital klage lønnskompensasjon

--- src/kategorier/kategorier.ts
+++ src/kategorier/kategorier.ts
@@ -46,7 +46,7 @@ export const INNGANG_KATEGORIER: InngangKategori[] = [
                 temaKey: TemaKey.PER,
                 titleKey: 'LONNSKOMPENSASJON',
                 allowsAnke: false,
-                digitalKlage: [],
+                digitalKlage: [EnvString.PROD, EnvString.DEV, EnvString.LOCAL],
                 digitalKlageFullmakt: false,
                 path: 'lonnskompensasjon',
                 mailAnkeUrl: {
