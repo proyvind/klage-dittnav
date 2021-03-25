79641cac2d8713e4b409e9c1f18ad2b301465939
Marianne Hval <marianne.hval@nav.no>
2021-03-25 13:50:48 +0100

    Digital klage for SU Alder

--- src/kategorier/kategorier.ts
+++ src/kategorier/kategorier.ts
@@ -592,7 +592,7 @@ export const INNGANG_KATEGORIER: InngangKategori[] = [
                 temaKey: TemaKey.SUP,
                 titleKey: 'SUPPLERENDE_STONAD',
                 allowsAnke: true,
-                digitalKlage: [],
+                digitalKlage: [EnvString.PROD, EnvString.DEV, EnvString.LOCAL],
                 digitalKlageFullmakt: false,
                 path: 'supplerende-stonad',
                 mailKlageUrl: {
